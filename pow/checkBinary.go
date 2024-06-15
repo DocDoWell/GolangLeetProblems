ecd6dbfd6c4adbec7e56499260879004b46039e0
DocDoWell <dandyowl84@gmail.com>
2024-06-15 14:32:55 +0100

    import added to checkBinary.go

--- pow/checkBinary.go
+++ pow/checkBinary.go
@@ -1,5 +1,7 @@
 package pow
 
+import "fmt"
+
 func output(){
     var a uint16 = 12
 	var b uint16 = 213
@@ -11,7 +13,7 @@ func output(){
 	fmt.Printf("Number %d in binary is %b\n", a & b , a& b)
 
 	fmt.Printf("Number %d in binary is %b\n", a << 1, a << 1)
-// 11010101 << 1  = 110101010 (We shift it once)
+
 
 	fmt.Printf("Number %d in binary is %b\n", a << 2, a << 2)	
 
